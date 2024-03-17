df913999c5e9ee7d20bb70a192c3639528d2727e
Ildaron <de.ikar@yandex.ru>
2024-03-17 22:45:44 +0000

    Update Grap_plot_alpha.py

--- 1.GUI/Grap_plot_alpha.py
+++ 1.GUI/Grap_plot_alpha.py
@@ -5,7 +5,7 @@ import numpy as np
 import pywt
 
 # Load the Excel file into a DataFrame
-df = pd.read_excel("output1.xlsx")
+df = pd.read_excel("output3.xlsx")
 
 # Assuming you want to visualize data from columns data_1ch_test to data_8ch_test
 columns_to_visualize = ['data_1ch_test', 'data_2ch_test', 'data_3ch_test', 'data_4ch_test', 'data_5ch_test', 'data_6ch_test', 'data_7ch_test', 'data_8ch_test']
